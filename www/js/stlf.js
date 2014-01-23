126d4c4c936c61f1f7a0ef1a657e3766b31c7050
Ben Pedersen <peder541@umn.edu>
2014-01-23 13:02:15 -0600

    stlf app

--- www/js/stlf.js
+++ www/js/stlf.js
@@ -388,6 +388,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 // PhoneGap is loaded and it is now safe to make calls PhoneGap methods
 function onDeviceReady() {
+	alert('ready');
 	// Register the event listener
    document.addEventListener("backbutton", onBackKeyDown, false);
 }
