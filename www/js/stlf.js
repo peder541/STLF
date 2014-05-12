f0e63a358b5777ce259361193172e51eafc64791
Ben Pedersen <peder541@umn.edu>
2014-05-11 22:45:39 -0500

    app

--- www/js/stlf.js
+++ www/js/stlf.js
@@ -388,7 +388,7 @@ $(document).ready(function() {
 			default:
 				var $this = $(this);
 				if ($this.parent().is('#fb-login')) {
-					if (FB) alert('hi');
+					if (FB) alert(FB.getAccessToken.toString());
 					/* Login Method 1 (auto) */
 					FB.Event.unsubscribe('auth.statusChange', updateStatusCallback);
 					FB.Event.subscribe('auth.statusChange', updateStatusCallback);
